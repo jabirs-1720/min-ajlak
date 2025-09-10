5825b8b9d53f516666134b29be5e66d59cb078a9
Jabir ahmed <jabirs1720@gmail.com>
2025-09-10 22:19:36 +0300

    routing and consumers files

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,11 +1,19 @@
-import os
-
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+# routers
+import cooks.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            cooks.routing.websocket_urlpatterns
+        )
+    ),
 })
