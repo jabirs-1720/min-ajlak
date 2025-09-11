bfd9ce93d191700a07f1cce04e8c60bb07f7150d
jaber's <jabirs1720@gmail.com>
2025-09-11 21:35:52 +0300

    filtering

--- meals/views.py
+++ meals/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
@@ -12,3 +13,8 @@ class Meals(ModelViewSet):
     permission_classes = [AllowAny]
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+
+    # Filtering
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_fields = ['restaurant']
