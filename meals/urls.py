59ddb8c75d793e810051f9acef85b9def74f06d4
jaber's <jabirs1720@gmail.com>
2025-09-11 21:47:02 +0300

    Meals as a ListAPIView

--- meals/urls.py
+++ meals/urls.py
@@ -1,10 +1,15 @@
+from django.urls import path
+
 from rest_framework.routers import DefaultRouter
 
 from .views import Meals
 
 app_name = 'items'
 
-router = DefaultRouter()
-router.register('', Meals, 'meals')
+# router = DefaultRouter()
+# router.register('', Meals, 'meals')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+urlpatterns = [
+    path('', Meals.as_view(), name='meals')
+]
