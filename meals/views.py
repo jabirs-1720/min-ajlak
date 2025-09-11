5cfee1576e854b6af38e7da3990f6593767667ab
jaber's <jabirs1720@gmail.com>
2025-09-11 21:38:50 +0300

    Turning Meals from ModelViewSet to ListAPIView

--- meals/views.py
+++ meals/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 
 from .models import Meal
 from .serializers import MealSerializer
 
 # Create your views here.
 
-class Meals(ModelViewSet):
+class Meals(ListAPIView):
     permission_classes = [AllowAny]
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
 
     # Filtering
     filter_backends = [DjangoFilterBackend]
-
     filterset_fields = ['restaurant']
