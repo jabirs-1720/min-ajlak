a7b07ffbceae0c1a5c49be54245c86fb8e30b356
jaber's <jabirs1720@gmail.com>
2025-09-12 13:27:59 +0300

    Anony user restaurant bug fixed

--- meals/permissions.py
+++ meals/permissions.py
@@ -4,8 +4,8 @@ class IsMealRestaurantOwner(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        
-        return getattr(request.user, 'restaurant')
+
+        return request.user.is_authenticated and getattr(request.user, 'restaurant', None) is not None
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
