724186b234cdb8e37447045d0960b0ee8e1c912f
jaber's <jabirs1720@gmail.com>
2025-09-12 13:57:37 +0300

    bug fix

--- meals/permissions.py
+++ meals/permissions.py
@@ -5,7 +5,10 @@ class IsMealRestaurantOwner(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return request.user.is_authenticated and getattr(request.user, 'restaurant', None) is not None
+        if request.user.is_authenticated and getattr(request.user, 'restaurant', None) is not None:
+            return True
+        else:
+            return False
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
